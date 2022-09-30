deeb5704c80f502208ce15e5c4d0711c3b6924c0
agustingenoves <agustingenoves@gmail.com>
2022-09-30 10:28:25 -0300

    Agregue el modelo de permisos.

--- admin/src/models/seeds.py
+++ admin/src/models/seeds.py
@@ -2,17 +2,29 @@ from src.models import auth
 
 
 def run():
-    role_1 = auth.create_role(name='Administrador')
-    role_2 = auth.create_role(name='Operador')
-    role_3 = auth.create_role(name='Socio')
-    
+    """Hacemos un seed de informacion en la BBDD"""
+    perms = [
+        "member_index",
+        "member_new",
+        "member_destroy",
+        "member_update",
+        "member_show",
+    ]
+
+    members_perms = [auth.create_permission(perm) for perm in perms]
+
+    role_1 = auth.create_role(name="Administrador")
+    role_1.permissions = members_perms
+    role_2 = auth.create_role(name="Operador")
+    role_3 = auth.create_role(name="Socio")
+
     user_1 = auth.create_user(
         email="carlos.solari@gmail.com",
         username="Indio49",
         password="carlos.solari@gmail.com",
         first_name="Carlos",
         last_name="Solari",
-        roles=[role_1]
+        roles=[role_1],
     )
 
     user_2 = auth.create_user(
@@ -21,7 +33,7 @@ def run():
         password="skay.beili@gmail.com",
         first_name="Eduardo",
         last_name="Beilinson",
-        roles=[role_2]
+        roles=[role_2],
     )
 
     user_3 = auth.create_user(
@@ -31,5 +43,5 @@ def run():
         is_active=False,
         first_name="Nadia",
         last_name="Benitez",
-        roles=[role_2, role_3]
+        roles=[role_2, role_3],
     )
