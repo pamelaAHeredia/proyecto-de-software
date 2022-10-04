eee98a5508a7086edf32e3c46278af2a2ede9128
Leandro Lopez <leandro230599@outlook.com>
2022-10-04 19:51:12 -0300

    Solucionado problema en hasheo de contraseña y mejorado la vista del panel

--- admin/src/models/auth/__init__.py
+++ admin/src/models/auth/__init__.py
@@ -2,6 +2,7 @@ from src.models.database import db
 from src.models.auth.user import User
 from src.models.auth.role import Role
 from src.models.auth.permission import Permission
+from src.models.auth.utils import verify_pass
 
 
 def list_users():
@@ -34,4 +35,8 @@ def create_permission(*args):
 
 def find_user_by_mail_and_pass(email, password):
     """Función que retorna si existe un usuario que coincida el email y contraseña"""
-    return User.query.filter_by(email=email, password=password).first()
+    user = User.query.filter_by(email=email).first()
+    if user and verify_pass(user.password,password):
+        return user
+    else:
+        return None
