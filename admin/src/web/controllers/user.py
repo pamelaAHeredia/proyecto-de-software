8fb0ecc954d35346bfa22628fe1e899145b764f4
Pamela Heredia <pame.a.heredia@gmail.com>
2022-10-08 13:18:06 -0300

    Agregado servicio de usaurios

--- admin/src/web/controllers/user.py
+++ admin/src/web/controllers/user.py
@@ -1,86 +1,62 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for
-from src.models import auth
+from src.services.user import UserService
 from src.models.auth.utils import hash_pass
 
-# Se define Blueprint de Usuario
+
 user_blueprint = Blueprint("users", __name__, url_prefix="/users")
+
+service = UserService()
 
 
 @user_blueprint.get("/")
 def users_index():
     """Por metodo GET pide la lista total de usuarios al modelo y lo renderiza en la vista"""
-    users = auth.list_users()
-
+    users = service.list_users()
     return render_template("users/index.html", users=users)
 
 
-# Agregar un Usuario
-@user_blueprint.route("/add", methods = ['POST'])
+@user_blueprint.route("/add", methods=["POST"])
 def users_add():
-    mail = request.form.get("email")
-    userName = request.form.get("username")
-    if (auth.mail_not_exists(mail)):
-        if(auth.username_not_exists(userName)):
-            data_user = {
-                "email": mail,
-                "username": userName,
-                "password": hash_pass(request.form.get("email")),
-                "first_name": request.form.get("first_name"),
-                "last_name": request.form.get("last_name"),
-            }
-            auth.create_user(**data_user)
-            flash("Usuario creado con éxito!", "success")
-        else: 
-            flash("El nombre de usuario ya se encuentra registrado.", "danger") 
-    else: 
-        flash("El mail ingresado ya se encuentra registrado.", "danger")
 
-    return redirect(url_for("users.users_index"))  
-
-#Eliminar un usuario
-@user_blueprint.route("/delete/<id>")
-def users_delete(id):
-    auth.delete_user(id)
-    flash("Usuario eliminado exitosamente.", "success")
-    return redirect(url_for("users.users_index")) 
+    data_user = {
+        "email": request.form.get("email"),
+        "username": request.form.get("username"),
+        "password": hash_pass(request.form.get("email")),
+        "is_active": True,
+        "first_name": request.form.get("first_name"),
+        "last_name": request.form.get("last_name"),
+        "blocked": False,
+    }
+    added_user = service.create_user(**data_user)
+    if added_user:
+        flash("Usuario creado con éxito!", "success")
+    else:
+        flash("El coso ya está guardado", "danger")
+    return redirect(url_for("users.users_index"))
 
 
-#actualización de datos de un usuario
-@user_blueprint.route("/update/<id>", methods = ['POST', 'GET'])
+@user_blueprint.route("/update/<id>", methods=["POST", "GET"])
 def users_update(id):
-    user = auth.find_user(id)
+    user = service.find_user_by_id(id)
     if request.method == "POST":
-        mail = request.form.get("email")
-        userName = request.form.get("username")
-        
-        if (auth.find_user_byEmail(mail).id == int(id)):
-            if(auth.find_user_byUsername(userName).id == int(id)):
-                data_user = {
-                    "email": mail,
-                    "username": userName,
-                    "first_name": request.form.get("first_name"),
-                    "last_name": request.form.get("last_name"),
-                }
-                
-                auth.update_user(id, **data_user)
-                flash("Usuario actualizado con éxito!", "success")
-                return render_template("users/update.html", user=user)
-            else: 
-                flash("El nombre de usuario ya se encuentra registrado.", "danger")
-                return render_template("users/update.html", user=user) 
-        else: 
-            flash("El mail ingresado ya se encuentra registrado.", "danger")
-            return render_template("users/update.html", user=user) 
+        data_user = {
+            "email": request.form.get("email"),
+            "username": request.form.get("username"),
+            "first_name": request.form.get("first_name"),
+            "last_name": request.form.get("last_name"),
+        }
+
+        updated = service.update_user(id, **data_user)
+        if updated:
+            flash("Usuario actualizado con éxito!", "success")
+        else:
+            flash("El coso ya existe", "danger")
+        return render_template("users/update.html", user=user)
     else:
         return render_template("users/update.html", user=user)
 
-#Buscar un usuario
 
-@user_blueprint.route("/search", methods = ['GET'])
-def users_search():
-    input = request.form.get("value")
-
-    if(input == "activo"):
-        return "búsqueda activo/inactivo"
-    else:
-        return "busqueda por mail?"
+@user_blueprint.route("/block_user/<id>", methods=["GET"])
+def users_block(id):
+    service.block_user(id)
+    return redirect(url_for("users.users_index"))
