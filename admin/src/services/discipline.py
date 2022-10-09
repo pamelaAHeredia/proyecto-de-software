ff8d236a5ca461536c4802eb867baba9477d5a96
Agustin Ignacio Genoves <agustingenoves@hotmail.com>
2022-10-09 19:18:10 -0300

    Se dcoumento el servicio de disciplinas.

--- admin/src/services/discipline.py
+++ admin/src/services/discipline.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from typing import List
 
 from src.models.database import db
 from src.models.club.discipline import Discipline
@@ -7,7 +8,10 @@ from src.services.paginator import Paginator
 
 
 class DisciplineService:
-    """Clase que representa el servicio para el manejo de disciplinas"""
+    """
+    Clase que representa el servicio para el manejo de disciplinas.
+    Implementa el patron singleton para que solo se instancie una vez en el sistema.
+    """
 
     _instance = None
 
@@ -16,29 +20,53 @@ class DisciplineService:
             cls._instance = super(DisciplineService, cls).__new__(cls)
         return cls._instance
 
-    def list_disciplines(self):
-        """
-        Función que retorna la lista de todas las disciplinas cargadas en la Base de Datos
+    def list_disciplines(self) -> List[Discipline]:
+        """Función que retorna la lista de todas las disciplinas cargadas en la Base de Datos
+
+        Returns:
+          Lista de disciplinas
+
         """
         return Discipline.query.all()
 
-    def list_paginated_disciplines(self, page, items_per_page, endpoint):
-        """
-        Función que retorna el paginador con las disciplinas cargadas en el sistema.
+    def list_paginated_disciplines(
+        self, page: int, items_per_page: int, endpoint: str
+    ) -> Paginator:
+        """Función que retorna el paginador con las disciplinas cargadas en el sistema.
+
+        Args:
+           page: Numero de pagina.
+           items_per_page: cantidad de registros por página.
+           endpoint: endpoint para el armado del url_for.
+
+        Returns:
+           Un paginador.
         """
         disciplines = Discipline.query
         return Paginator(disciplines, page, items_per_page, endpoint)
 
     def create_discipline(
         self,
-        name,
-        category,
-        instructor_first_name,
-        instructor_last_name,
-        days_and_schedules,
-        amount,
-    ):
-        """Función que instancia una Disciplina, la agrega a la Base de Datos y la retorna"""
+        name: str,
+        category: str,
+        instructor_first_name: str,
+        instructor_last_name: str,
+        days_and_schedules: str,
+        amount: Decimal,
+    ) -> Discipline:
+        """Función que instancia una Disciplina, la agrega a la Base de Datos y la retorna
+
+        Args:
+           name: Nombre de la disciplina.
+           category: Categoria de la disciplina.
+           instructor_first_name: Nombre del instructor que dicta la disciplina.
+           instructor_last_name: Apellido del instructor que dicta la disciplina.
+           days_and_schedule: Días y horarios en que se da la disciplina.
+           amount: Monto a pagar por practicar la disciplina.
+
+        Returns:
+           Una disciplina.
+        """
 
         if Decimal(amount) < 0:
             raise database.AmountValueError()
@@ -59,5 +87,25 @@ class DisciplineService:
             raise database.ExistingData(message="ya existen en la base de datos")
         return discipline
 
-    def find_discipline(self, name, category):
+    def find_discipline(self, name: str, category: str) -> Discipline:
+        """Función que busca una disciplina por nombre y categoria y la retorna.
+        
+        Args:
+           name: Nombre de la disciplina a buscar.
+           category: Categoria de la disciplina a buscar.
+           
+        Returns:
+           Una disciplina.
+        """
         return Discipline.query.filter_by(name=name, category=category).first()
+
+    def find_discipline(self, id: int) -> Discipline:
+        """Función que busca una disciplina por id y la retorna.
+        
+        Args:
+           id: Id de la disciplina a buscar.
+        
+        Returns:
+           Una disciplina
+        """
+        return Discipline.query.filter_by(id=id).first()
