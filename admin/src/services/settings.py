891f1ba66be7c9254727a57fc5dc63c26ca37e05
Leandro Lopez <leandro230599@outlook.com>
2022-10-15 14:36:52 -0300

    Se agrego docstring a los metodos del servicio

--- admin/src/services/settings.py
+++ admin/src/services/settings.py
@@ -40,22 +40,29 @@ class SettingsService:
         db.session.commit()
 
     def get_settings(self):
+        """Metodo que obtiene todas las configuraciones"""
         return Settings.query.get(1)
     
     def get_items_per_page(self):
+        """Metodo que obtiene los elementos por pagina"""
         return Settings.query.get(1).items_per_page
 
     def get_enable_paytable(self):
+        """Metodo que obtiene si esta habilitado la tabla de pagos"""
         return Settings.query.get(1).enable_paytable
 
     def get_contact_info(self):
+        """Metodo que obtiene la informacion de contacto"""
         return Settings.query.get(1).contact_info
 
     def get_text_header_payment(self):
+        """Metodo que obtiene el texto de encabezado en el recibo de pago"""
         return Settings.query.get(1).text_header_payment
 
     def get_amount_monthly(self):
+        """Metodo que obtiene el monto base mensual"""
         return Settings.query.get(1).amount_monthly
 
     def get_percentage_surcharge(self):
+        """Metodo que obtiene el porcentaje de recarga"""
         return Settings.query.get(1).percentage_surcharge
