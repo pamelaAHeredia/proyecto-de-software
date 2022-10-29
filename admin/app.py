e730da83f263774bd1bf747a2df7a6584863d358
Leandro Adrian Lopez <leandro230599@outlook.com>
2022-10-28 22:04:50 -0300

    Cambiada env para produccion

--- admin/app.py
+++ admin/app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 static_folder = Path(__file__).parent.absolute().joinpath("src/web/public")
 
-app = create_app(static_folder=static_folder)
+app = create_app(env='production', static_folder=static_folder)
 
 def main():
     app.run()
