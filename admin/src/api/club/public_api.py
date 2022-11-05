5942687ea80f703c3bda6b1259644a63b9c36dae
Pamela Heredia <pame.a.heredia@gmail.com>
2022-11-05 19:38:38 -0300

    agregado: CORS, cross_origin

--- admin/src/api/club/public_api.py
+++ admin/src/api/club/public_api.py
@@ -2,12 +2,13 @@ from flask import Blueprint
 from flask import jsonify
 
 from src.services.discipline import DisciplineService
+from flask_cors import cross_origin 
 
 
 service = DisciplineService()
 public_api_blueprint = Blueprint("club_api", __name__, url_prefix="/api/club")
 
-
+@cross_origin
 @public_api_blueprint.get("/disciplines")
 def discipline_list():
     disciplines = service.api_disciplines()
