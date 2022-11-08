ee73203f78cd0fce6bd83112bce754e3191a6fea
César Amiconi <cesar.amiconi@gmail.com>
2022-11-08 20:52:31 -0300

    Formateo de codigo

--- portal/src/views/Disciplines.vue
+++ portal/src/views/Disciplines.vue
@@ -1,53 +1,50 @@
 <template>
-    <div class="container">
-        <div class="table-responsive">
-
-            <table class="table">
-                <thead class="table-light">
-                    <td>Nombre</td>
-                    <td>Categorìa</td>
-                    <td>Horario</td>
-                    <td>Instructor</td>
-                </thead>
-                <tbody>
-                    <tr v-for="discipline in disciplines" :key="discipline.id">
-                        <th>{{ discipline.name }}</th>
-                        <th>{{ discipline.category }}</th>
-                        <th>{{ discipline.days_and_schedules }}</th>
-                        <th>{{ discipline.instructor }}</th>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+  <div class="container">
+    <div class="table-responsive">
+      <table class="table">
+        <thead class="table-light">
+          <td>Nombre</td>
+          <td>Categorìa</td>
+          <td>Horario</td>
+          <td>Instructor</td>
+        </thead>
+        <tbody>
+          <tr v-for="discipline in disciplines" :key="discipline.id">
+            <th>{{ discipline.name }}</th>
+            <th>{{ discipline.category }}</th>
+            <th>{{ discipline.days_and_schedules }}</th>
+            <th>{{ discipline.instructor }}</th>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+import axios from "axios";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 export default {
-    data(){
-        return {
-            disciplines:null
-        }
+  data() {
+    return {
+      disciplines: null,
+    };
+  },
+  mounted() {
+    this.getDisciplines();
+  },
+  methods: {
+    getDisciplines() {
+      axios
+        .get("http://127.0.0.1:5000/api/club/disciplines")
+        .then((response) => {
+          this.disciplines = response.data;
+        })
+        .catch((e) => console.log(e));
     },
-    mounted(){
-        this.getDisciplines(); 
-    },
-    methods: {
-
-        getDisciplines(){ 
-            axios.get('http://127.0.0.1:5000/api/club/disciplines')
-                .then(response => {
-                    this.disciplines = response.data
-                })
-                .catch(e => console.log(e))
-        }
-    }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
