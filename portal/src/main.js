c9b95a2aec6709dc1ab1891f6191fecfc2d2d85d
Pamela Heredia <pame.a.heredia@gmail.com>
2022-11-18 10:03:38 -0300

    comentarios eliminados

--- portal/src/main.js
+++ portal/src/main.js
@@ -8,16 +8,12 @@ import router from "./router";
 // import "./assets/main.css";
 
 const app = createApp(App);
-console.log("app creada");
 
 app.use(createPinia());
-console.log("pinia creada");
 const authStore = useAuthStore();
 app.use(router);
-console.log("En main.js, la app acaba de enganchar router");
 
 app.mount("#app");
-console.log("En main.js, la app acaba de ser Montada");
 
 const access_token = sessionStorage.getItem("token");
 if (access_token) {
