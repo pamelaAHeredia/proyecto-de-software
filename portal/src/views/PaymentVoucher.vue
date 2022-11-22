c7a5981c62365d24d0ddad1b601c6b1f3797166d
Leandro Lopez <leandro230599@outlook.com>
2022-11-22 16:36:25 -0300

    Probando fix de bug

--- portal/src/views/PaymentVoucher.vue
+++ portal/src/views/PaymentVoucher.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="container">
-    <input
-      accept=".png, .jpg, .jpeg,"
-      type="file"
-      id="fileInput"
-      ref="fileInput"
-      placeholder="edit me"
-      v-on:change="onChangeFileUpload(this)"
-    />
-    <input type="number" placeholder="Monto" />
-    <input type="text" placeholder="Descripcion" />
-    <button v-on:click="submitForm()">Upload</button>
+    <form v-on:submit.prevent="submitForm">
+      <input
+        accept=".png, .jpg, .jpeg,"
+        type="file"
+        id="fileInput"
+        ref="fileInput"
+        placeholder="edit me"
+        v-on:change="onChangeFileUpload(this)"
+      />
+      <input type="number" placeholder="Monto" />
+      <input type="text" placeholder="Descripcion" />
+      <button v-on:click="submitForm()">Upload</button>
+    </form>
   </div>
 </template>
 
@@ -29,13 +31,13 @@ export default {
     submitForm() {
       const formData = new FormData();
       formData.append("image", this.file);
-      //formData.append("image", this.file);
-      //formData.append("image", this.file);
-      //formData.append("image", this.file);
+      const headers = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
       axios
-        .post(`${PATH_SERVER}/api/me/payment/1`, {
-          formData,
-        })
+        .post(`${PATH_SERVER}/api/me/payment/1`, formData, headers)
         .then((response) => {
           console.log(response);
         })
