e13a7c32800a3eaa76ec48ffe12acfdb1e25e09c
agustingenoves <agustingenoves@gmail.com>
2022-11-24 16:58:30 -0300

    Fix de la API que devuelve los pagos de un socio determinado. Fix del controlador que guarda los pagos realizados por un socio en el back

--- admin/src/services/movement.py
+++ admin/src/services/movement.py
@@ -115,7 +115,7 @@ class MovementService:
     def get_movements(
         self,
         member: Member,
-        specific_date: datetime.date = TODAY,
+        specific_date: datetime.date = None,
         movement_type: str = None
     ) -> List[Movement]:
         """Retorna los movimientos.
@@ -136,9 +136,16 @@ class MovementService:
         #         specific_date + datetime.timedelta(days=1),
         #     )
         # ).order_by(Movement.date)
-        if movement_type:
+        if movement_type and not specific_date:
             member_movements = member.movements.filter(
                 Movement.movement_type == movement_type
+            ).order_by(Movement.date)
+        elif not movement_type and specific_date:
+            member_movements = member.movements.filter(
+                Movement.date.between(
+                    specific_date.replace(day=1),
+                    specific_date + datetime.timedelta(days=1),
+                )
             ).order_by(Movement.date)
         else:
             member_movements = member.movements.order_by(Movement.date)
@@ -272,8 +279,8 @@ class MovementService:
         return movement
 
 
-    def api_member_movements(self, member, movement_type):
+    def api_member_movements(self, member, specific_date):
         data = dict()
-        movements = self.get_movements(member=member, movement_type=movement_type)
+        movements = self.get_movements(member=member, specific_date=specific_date)
         data["movements"] = [m.resume() for m in movements]
         return data
