3fedbb54ec66f59c37ea4d671ca4fad020dd1cc1
Agustin Ignacio Genoves <agustingenoves@hotmail.com>
2022-11-26 19:04:03 -0300

    Pasaje a prod son cors en prod

--- portal/src/components/ContactInfo.vue
+++ portal/src/components/ContactInfo.vue
@@ -35,7 +35,7 @@
 
 <script>
 import axios from "axios";
-axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+// axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 const PATH_SERVER = import.meta.env.VITE_APP_PATH_API;
 
 export default {
